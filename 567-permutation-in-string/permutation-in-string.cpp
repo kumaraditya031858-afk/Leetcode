98d444dd6cdebbd793dd21bf82ebf5a6a1538616
Aditya  Prajapati <kumaraditya031858@gmail.com>
2026-06-10 15:59:29 +0530

    Time: 1063 ms (5.02%) | Memory: 60.6 MB (5.62%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+
+        if(s1.length() > s2.length())
+            return false;
+
+        sort(s1.begin(), s1.end());
+
+        int n = s1.length();
+
+        for(int i = 0; i <= s2.length() - n; i++) {
+
+            string temp = s2.substr(i, n);
+
+            sort(temp.begin(), temp.end());
+
+            if(temp == s1)
+                return true;
+        }
+
+        return false;
+    }
+};
